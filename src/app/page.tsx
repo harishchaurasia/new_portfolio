05894e15c9647dcb0c9971584f8feeed5a811294
Harish <hchaura1@asu.edu>
2024-07-22 21:29:25 +0530

    added nav after welconme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,38 @@ import Navs from '../components/Nav';
 
 export default function Home() {
   const [showBugs, setShowBugs] = useState(false);
+  const [showNav, setshowNav] = useState(false);
+  const [showGrid, setshowGrid] = useState(false);
 
+  // timeline for the bug spawning
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setShowBugs(true);
+  //   }, 4000); // 5 seconds delay for intro animation
+  // }, []);
+
+
+
+  // tinmeline for showing the nav bar
   useEffect(() => {
     setTimeout(() => {
-      setShowBugs(true);
-    }, 10); // 5 seconds delay for intro animation
+      setshowNav(true);
+    }, 4000); // 5 seconds delay for intro animation
+  }, []);
+
+  // timeline for shoing the grids
+  useEffect(() => {
+    setTimeout(() => {
+      setshowGrid(true);
+    }, 4000); // 5 seconds delay for intro animation
   }, []);
 
   return (
     <div className="relative">
-       {!showBugs && <Intro />}
+       {!showBugs && !showNav && <Intro />}
        {showBugs && <Grids />}
-      <Navs/>
+       {showNav && <Navs/>}
+
 
       <section id="home" className="content">
         {/* <h1 className="text-white">Home</h1> */}
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,13 +5,13 @@ import { gsap } from 'gsap';
 const Intro: React.FC = () => {
   useEffect(() => {
     const tl = gsap.timeline();
-    tl.to("#welcome-text", { opacity: 1, duration: 2, delay: 0.5 })
-      .to("#welcome-text", { opacity: 0, duration: 2, delay: 2 });
+    tl.to("#welcome-text", { opacity: 1, duration: 1.5, delay: 0.5 })
+      .to("#welcome-text", { opacity: 0, duration: 1.5, delay: 1 });
   }, []);
 
   return (
     <div className="flex items-center justify-center h-screen bg-black">
-      <h1 id="welcome-text" className="text-6xl text-red-800 opacity-0 font-imperfect">Welcome...</h1>
+      <h1 id="welcome-text" className="text-6xl text-white opacity-0 font-imperfect">Welcome...</h1>
     </div>
   );
 };
