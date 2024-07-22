696c89a29bff67091e31ceed6abac22854e12078
Harish <hchaura1@asu.edu>
2024-07-22 21:17:17 +0530

    added nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,26 @@ import { Link } from 'react-scroll';
 
 const Nav: React.FC = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 flex justify-between p-4 bg-[#1a1a1a] text-red-800">
+    <nav className="fixed top-0 left-0 right-0 flex justify-between p-4 bg-[#1a1a1a] text-white">
       <h1 className="font-imperfect items-center h-8 text-2xl ml-8">Harish</h1>
       <ul className="flex font-imperfect items-center h-8 text-sm space-x-4 mr-8">
         <li>
-          <Link to="home" smooth={true} duration={500} className="cursor-pointer hover:underline">
+          <Link to="home" smooth={true} duration={500} className="cursor-pointer ">
             Home
           </Link>
         </li>
         <li>
-          <Link to="about" smooth={true} duration={500} className="cursor-pointer hover:underline">
+          <Link to="about" smooth={true} duration={500} className="cursor-pointer ">
             About
           </Link>
         </li>
         <li>
-          <Link to="projects" smooth={true} duration={500} className="cursor-pointer hover:underline">
+          <Link to="projects" smooth={true} duration={500} className="cursor-pointer ">
             Projects
           </Link>
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500} className="cursor-pointer hover:underline">
+          <Link to="contact" smooth={true} duration={500} className="cursor-pointer ">
             Contact
           </Link>
         </li>
